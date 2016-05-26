daa5854e0482580dc0514fd1d855e31084fbd45d
Anatoly Pulyaevskiy <anatoly@sparkcentral.com>
2016-05-26 16:24:39 -0700

    Do not forget path separator

--- lib/templates/bin/console.dart
+++ lib/templates/bin/console.dart
@@ -8,7 +8,7 @@ import 'package:__projectName__/__projectName__.dart';
 
 Future main(List<String> args) async {
   var script = new File(Platform.script.path);
-  var projectRoot = script.parent.parent.path;
+  var projectRoot = script.parent.parent.path + Platform.pathSeparator;
   var bootstrap = new Bootstrap();
   var console = await bootstrap.createConsole(projectRoot: projectRoot);
   return console.run(args);
